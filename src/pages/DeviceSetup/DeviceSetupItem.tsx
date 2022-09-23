06f2e642fdb619335e626af8a1b3c1060fee57c3
lasergangers <celloadrian29@protonmail.com>
2022-09-23 16:19:43 +0700

    feat: implement input group in devicesetup

--- src/pages/DeviceSetup/DeviceSetupItem.tsx
+++ src/pages/DeviceSetup/DeviceSetupItem.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { VStack, Flex, Heading, Text } from "native-base";
 import { useWindowDimensions } from "react-native";
 
@@ -5,14 +6,14 @@ const DeviceSetupItem = ({ item, children }: any) => {
   const { width } = useWindowDimensions();
 
   return (
-    <VStack width={width}>
-      <Heading size="2xl">{item.title}</Heading>
-      <Text fontSize="lg">
-        {item.description}
-      </Text>
-      <Flex>
-        {children}
+    <VStack width={width} padding={25} justifyContent="center" space={50}>
+      <Flex alignItems="center">
+        <Heading size="2xl">{item.title}</Heading>
+        <Text fontSize="lg" textAlign="center">
+          {item.description}
+        </Text>
       </Flex>
+      <Flex width="full">{children}</Flex>
     </VStack>
   );
 };
